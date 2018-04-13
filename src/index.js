0c4baabdfd8ea3a16b4a102cfec659028ca6e7bc
Jeremy Friedman <jfriedm4@yahoo.com>
2018-04-13 17:08:23 -0400

    deployment stuff

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Routes from "./routes";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
